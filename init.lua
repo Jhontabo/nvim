01d54c6748a120b12702e6e660a595d7881dae6e
hacker <jhonse.tajumbina@gmail.com>
2025-10-19 21:01:12 -0500

    commit from arch

--- init.lua
+++ init.lua
@@ -1,19 +1,17 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- Sincroniza el portapapeles del sistema con el de Neovim
-vim.opt.clipboard = "unnamedplus"
-
--- Configura win32yank como el proveedor del portapapeles
-vim.g.clipboard = {
-  name = "win32yank",
-  copy = {
-    ["+"] = "win32yank.exe -i --crlf",
-    ["*"] = "win32yank.exe -i --crlf",
-  },
-  paste = {
-    ["+"] = "win32yank.exe -o --lf",
-    ["*"] = "win32yank.exe -o --lf",
-  },
-  cache_enabled = 1,
-}
+--
+-- vim.opt.clipboard = "unnamedplus"
+--
+-- vim.g.clipboard = {
+--   name = "win32yank",
+--   copy = {
+--     ["+"] = "win32yank.exe -i --crlf",
+--     ["*"] = "win32yank.exe -i --crlf",
+--   },
+--   paste = {
+--     ["+"] = "win32yank.exe -o --lf",
+--     ["*"] = "win32yank.exe -o --lf",
+--   },
+--   cache_enabled = 1,
+-- }
