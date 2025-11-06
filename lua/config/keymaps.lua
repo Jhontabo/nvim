a5db4655f4c46c7ba5988861b2e7d01a9706544a
Jhontabo <jhonse.tajumbina@gmail.com>
2025-11-05 21:32:23 -0500

    yazi plugin add

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,6 @@
 -- Add any additional keymaps here
 --
 
-vim.api.nvim_set_keymap("n", "-", ":Oil<CR>", { noremap = true, silent = true })
-
+--vim.api.nvim_set_keymap("n", "-", ":Oil<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "-", "<cmd>Yazi<CR>", { noremap = true, silent = true, desc = "open Yazi" })
 vim.api.nvim_set_keymap("n", "<C-a>", "gg^VG", { noremap = true, silent = true })
