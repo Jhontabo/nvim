7553b387f45ce02708cb71fe0ec8b4c097e772f8
Jhontabo <jhonse.tajumbina@gmail.com>
2026-01-26 14:58:38 -0500

    nvim working in wsl ubuntu

--- init.lua
+++ init.lua
@@ -1,17 +1,17 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 --
--- vim.opt.clipboard = "unnamedplus"
---
--- vim.g.clipboard = {
---   name = "win32yank",
---   copy = {
---     ["+"] = "win32yank.exe -i --crlf",
---     ["*"] = "win32yank.exe -i --crlf",
---   },
---   paste = {
---     ["+"] = "win32yank.exe -o --lf",
---     ["*"] = "win32yank.exe -o --lf",
---   },
---   cache_enabled = 1,
--- }
+vim.opt.clipboard = "unnamedplus"
+
+vim.g.clipboard = {
+  name = "win32yank",
+  copy = {
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf",
+  },
+  paste = {
+    ["+"] = "win32yank.exe -o --lf",
+    ["*"] = "win32yank.exe -o --lf",
+  },
+  cache_enabled = 1,
+}
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
-
---vim.api.nvim_set_keymap("n", "-", ":Oil<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "-", "<cmd>Yazi<CR>", { noremap = true, silent = true, desc = "open Yazi" })
+vim.api.nvim_set_keymap("n", "-", "<cmd>Yazi<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-a>", "gg^VG", { noremap = true, silent = true })
