3cb538e6698a31fc37df57567020dda811d8f686
hacker <jhonse.tajumbina@gmail.com>
2026-03-11 16:48:24 -0500

    feat: add nvim configuration with lazyvim setup

---
+++ lua/plugins/flutter.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-flutter/flutter-tools.nvim",
+  ft = { "dart" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "stevearc/dressing.nvim",
+  },
+  config = function()
+    require("flutter-tools").setup({})
+  end,
+}
