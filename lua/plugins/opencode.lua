368595bcda7a28b85d9aad0ed5f9cbd43670eb04
Jhontabo <jhonse.tajumbina@gmail.com>
2026-04-06 21:03:37 -0500

    feat(opencode): use Space as leader for keymaps

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -6,9 +6,9 @@ return {
 
     vim.o.autoread = true
 
-    vim.keymap.set({ "n", "x" }, "<C-o>", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
-    vim.keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end, { desc = "Execute opencode action…" })
-    vim.keymap.set({ "n", "t" }, "<C-.>", function() require("opencode").toggle() end, { desc = "Toggle opencode" })
+    vim.keymap.set({ "n", "x" }, "<Space>o", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
+    vim.keymap.set({ "n", "x" }, "<Space>x", function() require("opencode").select() end, { desc = "Execute opencode action…" })
+    vim.keymap.set({ "n", "t" }, "<Space>.", function() require("opencode").toggle() end, { desc = "Toggle opencode" })
 
     vim.keymap.set({ "n", "x" }, "go", function() return require("opencode").operator("@this ") end, { desc = "Add range to opencode", expr = true })
     vim.keymap.set("n", "goo", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Add line to opencode", expr = true })
