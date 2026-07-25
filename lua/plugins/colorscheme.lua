c631e753620612c3803ab44f5ec7e38028a6e14c
Jhontabo <jhonse.tajumbina@gmail.com>
2026-07-25 12:50:48 -0500

    feat: update nvim config - colorscheme and cleanup DAP

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,8 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      style = "moon",
-      transparent = true,
-    },
-  },
-  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "catppuccin",
     },
   },
 }
